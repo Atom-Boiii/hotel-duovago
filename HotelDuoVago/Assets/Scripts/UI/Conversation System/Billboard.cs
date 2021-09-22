f12b37924e3cdbcec5cd36db4a7aa8b41d151413
KoningSherwood <ramonzwiep@hotmail.com>
2021-09-22 14:33:23 +0200

    Conversation System in Hidde Scene

--- HotelDuoVago/Assets/Scripts/UI/Conversation System/Billboard.cs
+++ HotelDuoVago/Assets/Scripts/UI/Conversation System/Billboard.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public Transform lookAtThis;
+    public GameObject findRig;
+    public string vrRigName;
+
+    public void Start()
+    {
+        findRig = GameObject.Find(vrRigName);
+        lookAtThis = findRig.transform;
+    }
     void LateUpdate()
     {
         transform.rotation = Quaternion.LookRotation(transform.position - lookAtThis.position);
